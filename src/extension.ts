e161fa558540f21eeecf2da77798cde4e4bc64ba
Quinn Slack <qslack@qslack.com>
2020-04-16 19:56:54 -0700

    show CSV download link

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as sourcegraph from 'sourcegraph'
+import { Base64 } from 'js-base64'
 
 /**
  * A subset of the SearchResult type defined in the Sourcegraph GraphQL API.
@@ -16,20 +17,20 @@ interface SearchResult {
     }
 }
 
-/** Entrypoint for the extension. */
-export function activate(context: sourcegraph.ExtensionContext): void {
-    sourcegraph.commands.registerCommand(
-        'searchExport.exportSearchResultsToCSV',
-        async (query: string): Promise<void> => {
-            const {
-                data,
-                errors,
-            }: {
-                data: { search: { results: { results: SearchResult[] } } }
-                errors?: { message: string }[]
-            } = await sourcegraph.commands.executeCommand(
-                'queryGraphQL',
-                `
+export function activate(ctx: sourcegraph.ExtensionContext): void {
+    ctx.subscriptions.add(
+        sourcegraph.commands.registerCommand(
+            'searchExport.exportSearchResultsToCSV',
+            async (query: string): Promise<void> => {
+                const {
+                    data,
+                    errors,
+                }: {
+                    data: { search: { results: { results: SearchResult[] } } }
+                    errors?: { message: string }[]
+                } = await sourcegraph.commands.executeCommand(
+                    'queryGraphQL',
+                    `
                 query SearchResults($query: String!) {
                     search(query: $query) {
                         results {
@@ -55,33 +56,55 @@ export function activate(context: sourcegraph.ExtensionContext): void {
                     }
                 }
                 `,
-                {
-                    // Add a large count: to ensure we get all results.
-                    query: `${query} count:99999999`,
+                    {
+                        // Add a large count: to ensure we get all results.
+                        query: `${query} count:99999999`,
+                    }
+                )
+                if (errors) {
+                    throw new Error(
+                        `Search error: ${errors.map(e => e.message).join(',')}`
+                    )
                 }
-            )
-            if (errors) {
-                throw new Error(
-                    `Search error: ${errors.map(e => e.message).join(',')}`
+
+                // TODO: This CSV generation is not robust.
+                const results = data.search.results.results
+                const csvData = [
+                    [
+                        'Repository',
+                        'Repository external URL',
+                        'File path',
+                        'File URL',
+                        'File external URL',
+                    ],
+                    ...results.map(r =>
+                        [
+                            r.repository.name,
+                            r.repository.externalURLs[0]?.url,
+                            r.file.path,
+                            new URL(
+                                r.file.canonicalURL,
+                                sourcegraph.internal.sourcegraphURL
+                            ).toString(),
+                            r.file.externalURLs[0]?.url,
+                        ].map(s => JSON.stringify(s))
+                    ),
+                ]
+                    .map(row => row.join(','))
+                    .join('\n')
+                const base64Data = Base64.encodeURI(csvData)
+
+                const downloadFilename = `sourcegraph-search-export-${query.replace(
+                    /[^\w]/g,
+                    '-'
+                )}.csv`
+
+                // Show the user a download link for the CSV.
+                sourcegraph.app.activeWindow?.showNotification(
+                    `Search results export is complete.\n\n<a href="data:text/csv;base64,${base64Data}" download="${downloadFilename}"><strong>Download CSV</strong></a>`,
+                    sourcegraph.NotificationType.Success
                 )
             }
-
-            // TODO: This CSV generation is not robust.
-            const results = data.search.results.results
-            const csvData = results
-                .map(r =>
-                    [
-                        r.repository.name,
-                        r.repository.externalURLs[0]?.url,
-                        r.file.path,
-                        r.file.canonicalURL,
-                        r.file.externalURLs[0]?.url,
-                    ].join(',')
-                )
-                .join('\n')
-
-            // Display the CSV in the browser devtools console.
-            console.log(csvData)
-        }
+        )
     )
 }
